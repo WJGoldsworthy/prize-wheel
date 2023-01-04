9ae4b67a7d66715c18b9adb092d60601fedd10a2
William Goldsworthy <wjgoldsworthy@gmail.com>
2023-01-04 12:16:45 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import PrizeWheel from './components/PrizeWheel';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PrizeWheel from "./components/PrizeWheel";
+import "./App.css";
 
-const W2SWheel = () => (
-	<PrizeWheel title='W2S' />
-);
+const W2SWheel = () => <PrizeWheel title="W2S" />;
 
-const YungChipWheel = () => (
-	<PrizeWheel title='YungChip' />
-);
+const YungChipWheel = () => <PrizeWheel title="YungChip" />;
 
 function App() {
   return (
-  	<Fragment>
-	  	<BrowserRouter>
-	  		<Switch>
-		      <Route path='/W2S' component={W2SWheel} />
-			  <Route path='/yungchip' component={YungChipWheel} />
-			  <Route path='/' component={PrizeWheel} />
-		    </Switch>
-	    </BrowserRouter>
+    <Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/W2S" component={W2SWheel} />
+          <Route path="/yungchip" component={YungChipWheel} />
+          <Route path="/" component={PrizeWheel} />
+        </Switch>
+      </BrowserRouter>
     </Fragment>
   );
 }
-
 
 export default App;
